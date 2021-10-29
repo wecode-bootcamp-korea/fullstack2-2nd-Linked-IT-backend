a95850407da57f2716f4ed026e45b0727232541c
minjamie <81890292+minjamie@users.noreply.github.com>
2021-10-30 02:19:47 +0900

    Delete index.js

--- src/models/index.js
+++
@@ -1,8 +0,0 @@
-import commentDao from './commentDao';
-import employmentDao from './employmentDao';
-import mainDao from './mainDao';
-import profileDao from './profileDao';
-import userDao from './userDao';
-import searchDao from './searchDao';
-
-export { commentDao, employmentDao, mainDao, profileDao, userDao, searchDao };
