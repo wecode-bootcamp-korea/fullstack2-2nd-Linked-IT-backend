f7ac5a287cb5916875911917d6effa6f7e5d4a53
minjamie <81890292+minjamie@users.noreply.github.com>
2021-11-01 15:21:27 +0900

    [김민재] 채용 공고 리스트 Feature/employment list (#20)
    * first commit
    
    * Modified prisma V0
    
    * Deleted : 초기세팅 파일 삭제
    
    * Deleted : 초기세팅 파일 삭제
    
    * Modifed: prisma ENUM 테이블 추가 및 오타 수정
    
    * Add: 초기 세팅 적용 안된  파일들 추가
    
    * delete: 프리즈마 올라간 부분 삭제
    
    * Modifid: prisma migrate 전 readFil 추자
    
    * Add: 채용공고 디테일 부분 api 추가
    
    * Modified: 채용 공고 리스트 및 디테일 작업 수정 및 완료
    
    * Delete index.js
    
    * Delete schema.prisma
    
    * Delete index.js
    
    * Delete index.js
    
    * Add: 프리즈마 깃이그노어 파일에 추가
    
    Co-authored-by: jinsung <jsung1103@gmail.com>

---
+++ src/controllers/index.js
@@ -0,0 +1,15 @@
+import employmentController from './employmentController';
+// import commentController from './commentController';
+// import mainController from './mainController';
+// import profileController from './profileController';
+// import userController from './userController';
+// import searchController from './searchController';
+
+export {
+  employmentController,
+  // commentController,
+  // mainController,
+  // profileController,
+  // userController,
+  // searchController
+};
