4eb807b7a47c3c9a5a38cd718646491d066c6851
minjaekim <minjae2246@gmail.com>
2021-11-02 00:03:48 +0900

    Add: 초기세팅 index파일 추가

---
+++ src/models/index.js
@@ -0,0 +1,15 @@
+import employmentDao from './employmentDao';
+// import commentDao from './commentDao';
+// import mainDao from './mainDao';
+// import profileDao from './profileDao';
+// import userDao from './userDao';
+// import searchDao from './searchDao';
+
+export {
+  employmentDao,
+  // commentDao,
+  // mainDao,
+  // profileDao,
+  // userDao,
+  // searchDao
+};
---
+++ src/services/index.js
@@ -0,0 +1,15 @@
+import employmentService from './employmentService';
+// import commentService from './commentService';
+// import mainService from './mainService';
+// import profileService from './profileService';
+// import userService from './userService';
+// import searchService from './searchService';
+
+export {
+  employmentService,
+  // commentService,
+  // mainService,
+  // profileService,
+  // userService,
+  // searchService
+};
