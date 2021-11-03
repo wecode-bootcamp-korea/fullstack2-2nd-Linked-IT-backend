d70b625b85078eb57ac2423cb9e0de60654e4740
Brad Han <imgum9@gmail.com>
2021-11-04 01:44:44 +0900

    Modify: 에러 발생하는 로직 주석처리

--- src/routes/friendRouter.js
+++ src/routes/friendRouter.js
@@ -3,7 +3,7 @@ import { friendController } from '../controllers';
 
 const router = express.Router();
 
-router.get('/:userid', friendController.getFriend);
+// router.get('/:userid', friendController.getFriend);
 router.get('/my', friendController.getMyFriendList);
 
 export default router;
