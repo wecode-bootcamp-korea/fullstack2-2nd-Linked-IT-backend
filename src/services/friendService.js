fb047fc2b6e65c806c0dfd11ed71567cd954f7b5
minjaekim <minjae2246@gmail.com>
2021-11-04 23:19:28 +0900

    Fix

--- src/services/friendService.js
+++ src/services/friendService.js
@@ -7,9 +7,6 @@ const getTotalFriendCount = async (userId) => {
 const getFriendListByStatus = async (userId, friendStatusId) => {
   return await friendDao.getFriendListByStatus(userId, friendStatusId);
 };
-const getFriend = async (userId) => {
-  return await friendDao.getFriend(userId);
-};
 
 const addFriend = async (userInfo) => {
   return await friendDao.addFriend(userInfo);
@@ -22,14 +19,10 @@ const deleteFriend = async (userInfo) => {
 const updateFriend = async (userInfo) => {
   return await friendDao.updateFriend(userInfo);
 };
-const getFriend = async (userId) => {
-  return await friendDao.getFriend(userId);
-};
 
 export default {
   getTotalFriendCount,
   getFriendListByStatus,
-  getFriend,
   addFriend,
   deleteFriend,
   updateFriend,
